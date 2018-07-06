53ae8b9806f0430a2786611301023a064150dd8a
Viswa <viswanath3344@gmail.com>
2018-07-06 12:03:28 +0530

    Initial Commit

---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by Ming-En Liu on 06/07/18.
+//  Copyright © 2018 Vedas labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
